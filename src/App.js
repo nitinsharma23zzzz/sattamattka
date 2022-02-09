067781ed954a4da8f84a09984804b73eddbc2602
nitin <nitinsharma23zzzz@gmail.com>
2022-02-09 20:50:39 +0530

    Added background colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from "react-bootstrap";
+import React from 'react';
 import {Header} from "./components/Header";
+import {Cards} from "./components/Cards";
 
 function App() {
   return (
-    <div className="container">
+    <div>
         <Header />
-        <Card bg="primary"
-              text="white"
-              className='mb-2'
-              style={{ width: '18rem' }}
-        >
-          <Card.Header>Name</Card.Header>
-          <Card.Body>
-            <Card.Title> Card Title </Card.Title>
-            <Card.Text>Game</Card.Text>
-          </Card.Body>
-        </Card>
+        <Cards />
     </div>
   );
 }
