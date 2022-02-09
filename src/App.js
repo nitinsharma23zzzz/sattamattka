667c468b60c2f2b5a8a054303e1cc9431018a860
nitin <nitinsharma23zzzz@gmail.com>
2022-02-09 17:45:21 +0530

    attempt to publish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from "react-bootstrap";
+import {Header} from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        SATTA MATTKA
-      </header>
+    <div className="container">
+        <Header />
+        <Card bg="primary"
+              text="white"
+              className='mb-2'
+              style={{ width: '18rem' }}
+        >
+          <Card.Header>Name</Card.Header>
+          <Card.Body>
+            <Card.Title> Card Title </Card.Title>
+            <Card.Text>Game</Card.Text>
+          </Card.Body>
+        </Card>
     </div>
   );
 }
